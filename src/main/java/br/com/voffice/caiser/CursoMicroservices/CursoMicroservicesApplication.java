9c65c2def1e7af0984cdb7686d07d903d46e144f
andrerbandeirar <andrerbandeirar@gmail.com>
2018-06-14 18:19:23 -0300

    Spring boot inicialize

---
+++ src/main/java/br/com/voffice/caiser/CursoMicroservices/CursoMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package br.com.voffice.caiser.CursoMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroservicesApplication.class, args);
+	}
+}
